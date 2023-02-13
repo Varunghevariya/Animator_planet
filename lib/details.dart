39eb48cb5291c14124e1c84149bb180b882577b4
Varunghevariya <varunghevariya2003@gmail.com>
2023-02-13 16:27:09 +0530

    ||

--- lib/details.dart
+++ lib/details.dart
@@ -26,10 +26,10 @@ class _DetailsState extends State<Details> {
       appBar: AppBar(
         title: Text(
           "${s[0]}",
-          style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white70),
+          style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
         ),
         centerTitle: true,
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.black12,
         leading: GestureDetector(
           onTap: () {
             Navigator.pop(context);
@@ -43,9 +43,87 @@ class _DetailsState extends State<Details> {
       ),
       body: Container(
         decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("build/assets/image/sky.png"),
-                fit: BoxFit.fill)),
+          image: DecorationImage(
+              image: AssetImage("build/assets/image/sky.png"),
+              fit: BoxFit.fill),
+        ),
+        child: Stack(
+          children: [
+            Align(
+              alignment: (Alignment(0, -1)),
+              child: Image.asset(
+                "${s[2]}",
+                width: 280,
+              ),
+            ),
+            Align(
+              alignment: Alignment(0, -0.1),
+              child: Container(
+                margin: EdgeInsets.all(20),
+                height: h / 12,
+                width: w,
+                decoration: BoxDecoration(
+                    color: Colors.black12,
+                    border: Border.all(width: 5, color: Colors.white),
+                    borderRadius: BorderRadius.circular(50)),
+                alignment: Alignment.center,
+                child: Text(
+                  "${s[0]}",
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment(0, 0.15),
+              child: Container(
+                margin: EdgeInsets.all(20),
+                height: h / 12,
+                width: w,
+                decoration: BoxDecoration(
+                    color: Colors.black12,
+                    border: Border.all(width: 5, color: Colors.white),
+                    borderRadius: BorderRadius.circular(50)),
+                alignment: Alignment.center,
+                child: Text(
+                  "${s[1]}",
+                  style: const TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment(0,1),
+              child: Container(
+                margin: EdgeInsets.all(20),
+                height: h / 3.3,
+                width: w,
+                decoration: BoxDecoration(
+                    color: Colors.black12,
+                    border: Border.all(width: 5, color: Colors.white),
+                    borderRadius: BorderRadius.circular(50)),
+                child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8,right: 8),
+                      child: Text(
+                  "${s[3]}",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 2,fontSize: 17,
+                  ),
+                ),
+                    )),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -27,23 +27,23 @@ class _GelexyAppState extends State<GelexyApp> {
       'pic': 'build/assets/image/sun.png',
       'nick': 'The Center of Solar',
       'index': '1',
-      'detail':
-          'Sun, star around which Earth and the other components of the solar system revolve. It is the dominant body of the system, constituting more than 99 percent of its entire mass. The Sun is the source of an enormous amount of energy, a portion of which provides Earth with the light and heat necessary to support life.The Sun is classified as a G2 V star, with G2 standing for the second hottest stars of the yellow G class—of surface temperature about 5,800 kelvins (K)—and the V representing a main sequence, or dwarf, star, the typical star for this temperature class'
+      'details':
+          'Sun, star around which Earth and the other components of the solar system revolve. It is the dominant body of the system, constituting more than 99 percent of its entire mass. The Sun is the source of an enormous amount of energy, a portion of which provides Earth with the light and heat necessary to support life.The Sun is classified as a G2 V star.'
     },
     {
       'name': 'Mercury',
       'pic': 'build/assets/image/mercury.png',
       'nick': 'The Swift Planet',
       'index': '1',
-      'detail':
-          'What are 5 facts about Mercury Image result for mercury details At a distance of only about 1/3 of Earth\'s distance to Sun, Mercury is the first planet in our Solar System. With a diameter of only 4880 kilometers (3032 miles) Mercury is the smallest planet in the Solar System. It is barely bigger than our Moon. Not only is Mercury the smallest planet, it is also shrinking!'
+      'details':
+          'What are 5 facts about Mercury Image result for mercury details At a distance of only about 1/3 of Earth\'s distance to Sun, Mercury is the first planet in our Solar System. With a diameter of only 4880 kilometers (3032 miles) Mercury is the smallest planet in the Solar System. It is barely bigger than our Moon.'
     },
     {
       'name': 'Venus',
       'pic': 'build/assets/image/venus.png',
       'nick': 'Morning Star',
       'index': '2',
-      'detail':
+      'details':
           'Venus is the second planet from the Sun, and is Earth\'s closest neighbor in the solar system. Venus is the brightest object in the sky after the Sun and the Moon, and sometimes looks like a bright star in the morning or evening sky. The planet is a little smaller than Earth, and is similar to Earth inside.'
     },
     {
@@ -51,7 +51,7 @@ class _GelexyAppState extends State<GelexyApp> {
       'pic': 'build/assets/image/earth.png',
       'nick': 'The blue Planet',
       'index': '3',
-      'detail':
+      'details':
           'Earth consists of land, air, water and life. The land contains mountains, valleys and flat areas. The air is made up of different gases, mainly nitrogen and oxygen. The water includes oceans, lakes, rivers, streams, rain, snow and ice.'
     },
     {
@@ -59,15 +59,15 @@ class _GelexyAppState extends State<GelexyApp> {
       'pic': 'build/assets/image/mars.png',
       'nick': 'Red Planet',
       'index': '4',
-      'detail':
-          'Mars is the fourth planet from the Sun and the next planet beyond Earth. It is, on average, more than 142 million miles from the Sun. Mars turns on its axis more slowly than Earth does. So, a day on Mars is 24.6 hours.Mariner 4, which flew by Mars on July 14, 1965, found that Mars has an atmospheric pressure of only 1 to 2 percent of the Earth\'s. Temperatures on Mars average about -81 degrees F. However, temperatures range from around -220 degrees F. in the wintertime at the poles, to +70 degrees F.'
+      'details':
+          'Mars is the fourth planet from the Sun and the next planet beyond Earth. It is, on average, more than 142 million miles from the Sun. Mars turns on its axis more slowly than Earth does. So, a day on Mars is 24.6 hours.Mariner 4, which flew by Mars on July 14, 1965, found that Mars has an atmospheric pressure of only 1 to 2 percent of the Earth.'
     },
     {
       'name': 'Jupiter',
       'pic': 'build/assets/image/jupiter.png',
       'nick': 'The Gas Giant',
       'index': '5',
-      'detail':
+      'details':
           'Jupiter is the largest planet in the solar system. It is approximately 143,000kilometers about 89,000 miles) wide at its equator. Jupiter is so large that all of the other planets in the solar system could fit inside it. More than 1,300 Earths would fit inside Jupiter.',
     },
     {
@@ -75,7 +75,7 @@ class _GelexyAppState extends State<GelexyApp> {
       'pic': 'build/assets/image/saturn.png',
       'nick': 'Ringed Planet',
       'index': '6',
-      'detail':
+      'details':
           'Saturn is the sixth planet from the Sun and the second-largest planet in our solar system. Like fellow gas giant Jupiter, Saturn is a massive ball made mostly of hydrogen and helium. Saturn is not the only planet to have rings, but none are as spectacular or as complex as Saturn.'
     },
     {
@@ -83,7 +83,7 @@ class _GelexyAppState extends State<GelexyApp> {
       'pic': 'build/assets/image/uranus.png',
       'nick': 'The Ice Giant',
       'index': '7',
-      'detail':
+      'details':
           'Uranus is the seventh planet from the Sun, and has the third-largest diameter inour solar system. It was the first planet found with the aid of a telescope, Uranus was discovered in 1781 by astronomer William Herschel, although he originally thought it was either a comet or a star.',
     },
   ];
@@ -122,9 +122,8 @@ class _GelexyAppState extends State<GelexyApp> {
                         alignment: Alignment(-1, 0),
                         child: AnimatedRotation(
                           turns: 1,
-                          duration: Duration(minutes: 20),
+                          duration: Duration(hours: 40),
                           curve: Curves.ease,
-
                           child: Image.asset(
                             e["pic"],
                             width: 170,
